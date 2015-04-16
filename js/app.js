ed78f5e924ead0c0851abbfaca0713b919bd4ca8
joseterrera <joseterrera@yahoo.com>
2015-04-16 11:50:51 -0500

    new approach

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var Locations = function(data) {
-    this.name = data.name;
+    this.title = data.title;
     this.lat = data.lat;
     this.lng = data.lng;
 }
@@ -7,10 +7,10 @@ var Locations = function(data) {
 
 var initialLocations = [
 
-    {name: "Dollop",lat: 41.877124, lng: -87.629006 },
-    {name: "Magnificent Mile",lat: 41.894809, lng: -87.624214 },
-    {name: "Willis Towers",lat: 41.878876, lng: -87.635915 },
-    {name: "Millennium Park",lat: 41.882702, lng: -87.619394 }
+    {title: "Dollop",lat: 41.877124, lng: -87.629006 },
+    {title: "Magnificent Mile",lat: 41.894809, lng: -87.624214 },
+    {title: "Willis Towers",lat: 41.878876, lng: -87.635915 },
+    {title: "Millennium Park",lat: 41.882702, lng: -87.619394 }
 ]
 
 
@@ -22,14 +22,24 @@ var ViewModel = function() {
   this.locationsList = ko.observableArray(initialLocations);
 
   for (var i = 0; i < initialLocations.length; i++) {
-                var marker = new Locations(initialLocations[i].position, importantLocations[i].title, createMarker(importantLocations[i]));
+                var marker = new Locations(initialLocations[i].position, initialLocations[i].title, createMarker(initialLocations[i]));
                 self.locations.push(marker);
  
 
+  }
 }
 
 ko.applyBindings(new ViewModel());
 
+
+  function createMarker(loc) {
+            return new google.maps.Marker({
+                position: loc.position,
+                map: map,
+                title: loc.title
+            });
+        }
+var map;
  function initialize() {
         var mapCanvas = document.getElementById('map-canvas');
         var mapOptions = { 
@@ -37,6 +47,6 @@ ko.applyBindings(new ViewModel());
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
+      map = new google.maps.Map(mapCanvas, mapOptions)
       }
       google.maps.event.addDomListener(window, 'load', initialize);
